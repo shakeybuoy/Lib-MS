5725c1a8250f541cbb0f36bf11273211c57e45ef
shakeybuoy <abhi.sharma.0873@gmail.com>
2021-09-06 22:11:36 +0530

    lms v1.0

---
+++ library/admin/includes/footer.php
@@ -0,0 +1,12 @@
+   <section class="footer-section">
+       <div class="container">
+           <div class="row">
+               <div class="col-md-10">
+                   &copy; 2021 Library Management System| Designed by : <a
+                       href="http://linkedin.com/in/abhishek-8211">Abhishek Sharma</a> &
+                   Piyush
+               </div>
+
+           </div>
+       </div>
+   </section>
